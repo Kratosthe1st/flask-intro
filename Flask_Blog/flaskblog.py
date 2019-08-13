525689b601c23160357ea23f495969deb1bf3bba
Brian Musyoki <Musyokibr@gmail.com>
2019-08-13 23:10:22 +0300

    ture

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,14 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Brian Musyoki',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'August 10th 2019'
+    },
+
+    {
+        'author': 'Nancy Mumina',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 21st 2019'
+    },
+
+    {
+        'author': 'Thomas Mumina',
+        'title': 'Blog post 3',
+        'content': 'Third post content',
+        'date_posted': 'August 21st 2019'
+    }
+
+]
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Home Page!</h1>'
+    return render_template('home.html', data=posts)
+
 
 @app.route('/about')
 def about():
-    return '<h1>About Page!</h1>'
+    return render_template('about.html', title='ABout Bruh!')
+
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
